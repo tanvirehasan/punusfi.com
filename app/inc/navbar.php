d99fa37b95c7ff67c5993276cd9f6eec0e61e0c5
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-08 03:55:52 +0600

    Update navbar.php

--- app/inc/navbar.php
+++ app/inc/navbar.php
@@ -9,24 +9,32 @@
 					<span class="menu-text">Dashboard</span>
 				</a>
 			</div>
+
 			<div class="menu-item">
-				<a href="packge.php" class="menu-link">
-					<span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
-					<span class="menu-text">Investment</span>
+				<a href="helper.html" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-gem"></i></span>
+					<span class="menu-text">PURCHASE</span>
 				</a>
 			</div>
 
 			<div class="menu-item">
-				<a href="wallet.php" class="menu-link">
-					<span class="menu-icon"><i class="fas fa-wallet"></i></span>
-					<span class="menu-text">wallet</span>
+				<a href="helper.html" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-gem"></i></span>
+					<span class="menu-text">DEPOSIT</span>
 				</a>
 			</div>
 
 			<div class="menu-item">
-				<a href="team.php?tree=<?= UserData('user_name') ?>" class="menu-link">
-					<span class="menu-icon"><i class="fas fa-wallet"></i></span>
-					<span class="menu-text">Team</span>
+				<a href="helper.html" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-gem"></i></span>
+					<span class="menu-text">WITHDRAW</span>
+				</a>
+			</div>
+
+			<div class="menu-item">
+				<a href="helper.html" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-gem"></i></span>
+					<span class="menu-text">TRANSFER</span>
 				</a>
 			</div>
 
@@ -35,39 +43,84 @@
 					<span class="menu-icon">
 						<i class="bi bi-envelope"></i>
 					</span>
-					<span class="menu-text">Financial</span>
+					<span class="menu-text">INCOME DETAILS</span>
 					<span class="menu-caret"><b class="caret"></b></span>
 				</a>
 				<div class="menu-submenu">
 					<div class="menu-item">
 						<a href="deposit.php" class="menu-link">
-							<span class="menu-text">Funds Add</span>
+							<span class="menu-text">ROI</span>
 						</a>
 					</div>
 					<div class="menu-item">
 						<a href="sent_money.php" class="menu-link">
-							<span class="menu-text">Fund Transfer</span>
+							<span class="menu-text">REFERRAL</span>
 						</a>
 					</div>
 					<div class="menu-item">
 						<a href="deposit.php" class="menu-link">
-							<span class="menu-text">Funds Withdow</span>
+							<span class="menu-text">GENARATIONS</span>
 						</a>
 					</div>
+
 					<div class="menu-item">
 						<a href="email_detail.html" class="menu-link">
-							<span class="menu-text">Detail</span>
+							<span class="menu-text">RANK AND REWARDS </span>
+						</a>
+					</div>
+
+					<div class="menu-item">
+						<a href="email_detail.html" class="menu-link">
+							<span class="menu-text">ROYALITY </span>
 						</a>
 					</div>
 				</div>
 			</div>
 
 			<div class="menu-item">
-				<a href="helper.html" class="menu-link">
-					<span class="menu-icon"><i class="bi bi-gem"></i></span>
-					<span class="menu-text">Helper</span>
+				<a href="packge.php" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
+					<span class="menu-text">REFERRAL LIST</span>
 				</a>
 			</div>
+
+			<div class="menu-item">
+				<a href="packge.php" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
+					<span class="menu-text">TREE VIEW </span>
+				</a>
+			</div>
+
+			<div class="menu-item">
+				<a href="packge.php" class="menu-link">
+					<span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
+					<span class="menu-text">PACKAGE STATUS</span>
+				</a>
+			</div>
+
+			<div class="menu-item has-sub">
+				<a href="#" class="menu-link">
+					<span class="menu-icon">
+						<i class="bi bi-envelope"></i>
+					</span>
+					<span class="menu-text">SUPPORT</span>
+					<span class="menu-caret"><b class="caret"></b></span>
+				</a>
+				<div class="menu-submenu">
+					<div class="menu-item">
+						<a href="deposit.php" class="menu-link">
+							<span class="menu-text">NEW TICKET</span>
+						</a>
+					</div>
+					<div class="menu-item">
+						<a href="sent_money.php" class="menu-link">
+							<span class="menu-text">TICKET HISTORY</span>
+						</a>
+					</div>
+
+				</div>
+			</div>
+
 		</div>
 	</div>
 </div>
