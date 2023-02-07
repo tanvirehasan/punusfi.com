da160c259c6c22e6cd5c601eb37d6f05ae08d433
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-08 01:51:54 +0600

    Update config-theme.js

--- js/config-theme.js
+++ js/config-theme.js
@@ -43,7 +43,7 @@ const HomepageApp = {
             addonButtons: true,
             buttons: [{
                 name: 'Log in', // button name
-                url: '/signin.html', // button url
+                url: '/join.php', // button url
                 type: 'primary', // button type (default, primary, secondary, danger, text)
                 icon: 'sign-in-alt' // button icon, you can use all icons from here : https://fontawesome.com/icons?d=gallery&s=solid&m=free
             }]
