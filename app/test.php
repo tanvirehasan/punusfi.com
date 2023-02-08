762d55d870e71a285b1df91c1076e8779b9aa8f5
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-08 17:12:24 +0600

    Update test.php

--- app/test.php
+++ app/test.php
@@ -1,13 +1,14 @@
 <?php
-
-
-$to = "tanvirhasanbcse@gmail.com";
+$to = "recipient@example.com";
 $subject = "Test email using PHP";
 $message = "This is a test email message.";
-$headers = "From: team@punusfi.com";
+$headers = "From: team@punusfi.com" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion() . "\r\n" .
+    "SMTP-Host: mail.punusfi.com\r\n" .
+    "SMTP-Username: team@punusfi.com\r\n" .
+    "SMTP-Password: TaNVIR@@114\r\n";
 
 mail($to, $subject, $message, $headers);
-
 
 
 ?>
