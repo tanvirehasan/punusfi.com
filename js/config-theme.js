811e216dedae02eb60f2fb98de06d52f8a91524e
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-09 12:43:45 +0600

    new

--- js/config-theme.js
+++ js/config-theme.js
@@ -43,7 +43,7 @@ const HomepageApp = {
             addonButtons: true,
             buttons: [{
                 name: 'Log in', // button name
-                url: '/join.php', // button url
+                url: 'https://punusfi.com/app', // button url
                 type: 'primary', // button type (default, primary, secondary, danger, text)
                 icon: 'sign-in-alt' // button icon, you can use all icons from here : https://fontawesome.com/icons?d=gallery&s=solid&m=free
             }]
