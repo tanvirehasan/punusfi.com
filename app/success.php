5706a7f9a11e18cca7ad30fdf6b5fabd30dd6e0b
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-16 01:40:17 +0600

    Update success.php

--- app/success.php
+++ app/success.php
@@ -8,6 +8,6 @@ include "inc/navbar.php";
     <div class="row">
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h1 class="display-4">Success!</h1>
-            <p class="lead">Your Task Successfuly Done!</p>
+            <p class="lead">Your Task Successfully Done</p>
         </div>
-<?php include "inc/footer.php"; ?>
+        <?php include "inc/footer.php"; ?>
