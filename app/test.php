f851f6a3afe47df24eac06f903765302ab2ca269
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-17 12:58:11 +0600

    Update test.php

--- app/test.php
+++ app/test.php
@@ -4,6 +4,6 @@ $data = $conn->query("SELECT * FROM team_tree");
 
 while ($row = $data->fetch_object()) {
 
-    $conn->query("UPDATE users SET `placemnet_id`='$row->plecement_id', `placement`='A' WHERE user_name='$row->A' ");
+    $conn->query("UPDATE users SET `placemnet_id`='$row->plecement_id', `placement`='B' WHERE user_name='$row->B' ");
 
 }
