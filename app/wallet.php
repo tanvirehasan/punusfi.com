5392985fe53adb1fe9b433eafb66815ea2e6dac9
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-02-24 04:33:58 +0600

    Update wallet.php

--- app/wallet.php
+++ app/wallet.php
@@ -124,7 +124,7 @@ include "inc/navbar.php";
                     <div class="row align-items-center mb-2">
                         <div class="col-7">
                             <h3 class="mb-0">$
-                                <?php echo $rankwallet = is_float(Rankwallet());?>
+                                <?php echo $rankwallet = Rankwallet();?>
                             </h3>
                         </div>
                         <div class="col-5">
