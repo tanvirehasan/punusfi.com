b30e90915e8fd973c7d5bf8bfe41c1c9be50d2c1
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-03-04 03:18:19 +0600

    Update side_sell_abc.php

--- app/side_sell_abc.php
+++ app/side_sell_abc.php
@@ -8,7 +8,7 @@ include_once "rank.php";
 ?>
 
 <div class="modal-header bg-dark">
-    <h3>All Users/Active Users | Team's Sales ($) </h3>
+    <h3>All User/Active User | Team's Sales ($) </h3>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 
