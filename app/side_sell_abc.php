e04c066dc71212c4ba165a0c02506993d9bdf866
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-03-04 03:11:12 +0600

    Update side_sell_abc.php

--- app/side_sell_abc.php
+++ app/side_sell_abc.php
@@ -7,8 +7,8 @@ $puser_name = $_GET['user'];
 include_once "rank.php";
 ?>
 
-<div class="modal-header">
-    <img src="https://punusfi.com/img/logo.png" width="100">
+<div class="modal-header bg-dark">
+    <img src="https://punusfi.com/img/logo.png" width="60px">
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 
